5a6a93469a508bfe8f344807769ece5d76b8c369
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2023-12-03 17:24:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 clear
 apt-get update -y 
 apt-get install wget gzip xterm
-mkdir wordlists
+mkdir wordlists file_pcap
 cd wordlists
 wget https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz
 gzip -d rockyou.txt.gz
@@ -11,6 +11,6 @@ cd ../src
 chmod +x fwifi
 mv fwifi /usr/bin/
 echo ""
-echo "Installation complete."
-echo "To run FWIFI, type the command 'fwifi'."
+echo "FWIFI berhasil diinstal."
+echo "Untuk menjalankan FWIFI, ketikkan perintah 'fwifi'."
 echo ""
