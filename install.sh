75810538ccf17b05306ee8ad3f465eac3d112947
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2023-12-10 07:35:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ clear
 apt-get update -y 
 apt-get upgrade -y
 apt-get install wget gzip xterm
-mkdir -p wordlists
+mkdir -p wordlists pcap_file
 cd wordlists
 wget https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz
 gzip -d rockyou.txt.gz
