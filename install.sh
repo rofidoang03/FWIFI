e5353290bd1fe30ec5bee82cc5ef0a53b09faa2f
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2023-12-10 07:37:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,8 @@ mkdir -p wordlists # pcap_file
 cd wordlists
 wget https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz
 gzip -d rockyou.txt.gz
-cd ../src
 chmod +x fwifi
-mv fwifi /usr/bin/
+# mv fwifi /usr/bin/
 echo ""
 echo "Installation completed."
 echo ""
